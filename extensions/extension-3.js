c8c190836c9af2c916f6091976e4d4006d2251fb
Caleb Jaquith <caleb.jaquith@tealium.com>
2021-12-16 09:34:21 +0100

    Clean up docs

--- extensions/extension-3.js
+++ extensions/extension-3.js
@@ -23,7 +23,7 @@
 ;(function avoidGlobalScopeUnlessExplicit (window) {
   // set names for key objects and variables to make them easy to change if needed
 
-  var version = 'v1.0.0-beta-3'
+  var version = 'v1.0.0-alpha-1'
 
   /**
  * A window-scoped (global) object used to expose or define selected functionality.
