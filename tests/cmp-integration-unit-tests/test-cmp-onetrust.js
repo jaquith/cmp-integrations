97857dfa3b23325b8d02406d0f07bad73beb92f0
Caleb Jaquith <caleb.jaquith@tealium.com>
2021-12-23 10:36:55 +0100

    Update filenames

--- tests/cmp-integration-unit-tests/test-cmp-onetrust.js
+++ tests/cmp-integration-unit-tests/test-cmp-onetrust.js
@@ -3,7 +3,7 @@
 const stringFunctions = require('../helpers/stringFunctions.js')
 const tests = require('../helpers/cmp-base-tests')
 
-const cmpHelper = require('../helpers/cmp-specific/test-helper-onetrust-v2.js')
-const code = stringFunctions.getVanillaJsFile('extensions/extension-2-cmp-variants/extension-2-onetrust-v2.js')
+const cmpHelper = require('../helpers/cmp-specific/test-helper-onetrust.js')
+const code = stringFunctions.getVanillaJsFile('extensions/extension-2-cmp-variants/extension-2-onetrust.js')
 
 describe('the OneTrust integration', tests.getCmpTestSuite(code, cmpHelper))
